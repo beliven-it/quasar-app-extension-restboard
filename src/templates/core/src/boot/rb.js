8e1a9f47195470337a5c0c13f2ff51c26d972dc2
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-12-03 19:20:14 +0100

    fix: fully remove rb-vue-smart-components

--- src/templates/core/src/boot/rb.js
+++ src/templates/core/src/boot/rb.js
@@ -1,8 +1,6 @@
 import rb from 'rb-vue'
-import rbVueSmartComponents from 'rb-vue-smart-components'
 import resources from '../resources'
 
 export default ({ app }) => {
-  app.use(rbVueSmartComponents)
   app.use(rb, { resources })
 }
