ef58369ba93435bc00ded9d178d5bc3e4c6d3f2d
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2021-12-14 02:01:48 +0100

    refactor: handle user identity, profile page and logout

--- src/templates/core/src/layouts/MainLayout.vue
+++ src/templates/core/src/layouts/MainLayout.vue
@@ -1,11 +1,32 @@
 <template>
-  <rb-main-layout />
+  <rb-main-layout
+    :userIdentity="userIdentity"
+    @profile="onShowProfile"
+    @logout="onLogout"
+  />
 </template>
 
 <script>
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'MainLayout'
+  name: 'MainLayout',
+
+  computed: {
+    userIdentity () {
+      return this.$store.state.core.userIdentity
+    }
+  },
+
+  methods: {
+    onShowProfile () {
+      this.$router.push('/profile')
+    },
+
+    onLogout () {
+      this.$store.dispatch('core/logout')
+        .then(() => this.$router.push('/auth/login'))
+    }
+  }
 })
 </script>
