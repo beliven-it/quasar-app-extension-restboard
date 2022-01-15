e2e067b4a5ac5e5f9b380ef2ca669529ecd97fb5
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2022-01-15 15:31:37 +0100

    feat: add ResourceGrid page

---
+++ src/templates/core/src/pages/ResourceGrid.vue
@@ -0,0 +1,11 @@
+<template>
+  <rb-resource-grid-page padding />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageResourceGrid'
+})
+</script>
