edb5817fc029d9b718a5567271dc9ce56b1d0798
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2022-02-22 21:59:00 +0100

    docs: add reference tokenGetter

--- src/templates/examples/src/providers/data.js
+++ src/templates/examples/src/providers/data.js
@@ -1,3 +1,12 @@
 import createProvider from 'rb-data-provider-json-server'
 
-export default createProvider('https://jsonplaceholder.typicode.com')
+export default createProvider('https://jsonplaceholder.typicode.com', {
+  // Uncomment the following function to retrieve auth token
+  // from local or session storages:
+  // tokenGetter: () => {
+  //   return (
+  //     localStorage.getItem('rb-auth-token') ||
+  //     sessionStorage.getItem('rb-auth-token')
+  //   )
+  // }
+})
