b043eaee0ac9297165e698e63f4f34ffa4e99c74
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2022-10-28 12:36:58 +0200

    fix: don't install vuelentino in target projects

--- src/install.js
+++ src/install.js
@@ -4,40 +4,45 @@
  * Docs: https://quasar.dev/app-extensions/development-guide/install-api
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/InstallAPI.js
  */
-const path = require('path')
-const fs = require('fs')
+const path = require("path");
+const fs = require("fs");
 
-function installCoreModule (api) {
-  api.render('./templates/core')
+function installCoreModule(api) {
+  api.render("./templates/core");
 
-  restboardPackageJsonFilename = path.join(__dirname, '../package.json')
+  restboardPackageJsonFilename = path.join(__dirname, "../package.json");
   restboardPackageJson = JSON.parse(
     fs.readFileSync(restboardPackageJsonFilename)
-  )
+  );
 
-  api.extendPackageJson({
+  const deps = {
     dependencies: restboardPackageJson.dependencies,
-    devDependencies: restboardPackageJson.devDependencies
-  })
+    devDependencies: restboardPackageJson.devDependencies,
+  };
 
-  const extensionsFilename = api.resolve.app('quasar.extensions.json')
+  // Remove "vuelentino" from target deps bundle
+  delete deps.devDependencies.vuelentino;
+
+  api.extendPackageJson(deps);
+
+  const extensionsFilename = api.resolve.app("quasar.extensions.json");
   if (!fs.existsSync(extensionsFilename)) {
-    fs.writeFile(extensionsFilename, '{}')
+    fs.writeFile(extensionsFilename, "{}");
   }
 
-  api.extendJsonFile('quasar.extensions.json', {
-    'rb-ui': {}
-  })
+  api.extendJsonFile("quasar.extensions.json", {
+    "rb-ui": {},
+  });
 }
 
-function installExamplesModule (api) {
-  api.render('./templates/examples')
+function installExamplesModule(api) {
+  api.render("./templates/examples");
 }
 
 module.exports = function (api) {
-  installCoreModule(api)
+  installCoreModule(api);
 
   if (api.prompts.examples) {
-    installExamplesModule(api)
+    installExamplesModule(api);
   }
-}
+};
