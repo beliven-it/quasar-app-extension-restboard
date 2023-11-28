dca61dc9d29420db80620b40584d8b4e7850c0ce
Emanuele Bertoldi <emanuele.bertoldi@gmail.com>
2023-11-29 00:35:40 +0100

    fix: alternative index component

--- src/templates/examples/src/resources/users.js
+++ src/templates/examples/src/resources/users.js
@@ -1,24 +1,24 @@
-import { defineAsyncComponent } from 'vue'
-import { createResource } from 'rb-core-module'
-import { dataProvider } from '../providers'
+import { createResource } from "rb-core-module";
+import { defineAsyncComponent } from "vue";
+import { dataProvider } from "../providers";
 
 export default createResource({
-  name: 'users',
+  name: "users",
   provider: dataProvider,
-  displayAttr: 'name',
+  displayAttr: "name",
   schema: {
-    type: 'object',
+    type: "object",
     properties: {
       id: {
-        type: 'number'
+        type: "number",
       },
       name: {
-        type: 'string'
+        type: "string",
       },
       email: {
-        type: 'string'
-      }
-    }
+        type: "string",
+      },
+    },
   },
   // actions: {
   //   edit: {
@@ -29,28 +29,28 @@ export default createResource({
   //   }
   // },
   ui: {
-    icon: 'group',
+    icon: "group",
     // indexComponent: defineAsyncComponent(() =>
-    //   import('components/ResourceTable.vue')
+    //   import("pages/ResourceGridPage.vue")
     // ),
     // cardComponent: defineAsyncComponent(() =>
     //   import('components/ResourceCard.vue')
     // ),
     formComponent: defineAsyncComponent(() =>
-      import('components/UserForm.vue')
+      import("components/UserForm.vue")
     ),
     columns: [
       {
-        name: 'id'
+        name: "id",
       },
       {
-        name: 'name',
-        filterable: true
+        name: "name",
+        filterable: true,
       },
       {
-        name: 'email',
-        filterable: true
-      }
-    ]
-  }
-})
+        name: "email",
+        filterable: true,
+      },
+    ],
+  },
+});
